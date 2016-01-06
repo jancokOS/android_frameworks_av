e7aff94d2209b7e5efd328a00eafa71501e25341
Steve Kondik <steve@cyngn.com>
2016-01-05 16:45:30 -0800

    stagefright: Add deferred release option
     * Fill in stub methods to improve video encoding performance.
    
    Change-Id: I2bbf192e022043d161429c6cabf4a7d740802d20

--- media/libavextensions/stagefright/AVExtensions.h
+++ media/libavextensions/stagefright/AVExtensions.h
@@ -36,6 +36,7 @@
 #include <camera/android/hardware/ICamera.h>
 #include <media/mediarecorder.h>
 #include <media/stagefright/MediaCodecSource.h>
+#include <media/stagefright/MetaData.h>
 #include "ESQueue.h"
 
 namespace android {
@@ -45,7 +46,6 @@ struct MediaCodec;
 struct ALooper;
 class MediaExtractor;
 class AudioParameter;
-class MetaData;
 class CameraParameters;
 class MediaBuffer;
 class CameraSource;
@@ -149,8 +149,14 @@ struct AVUtils {
     virtual void addDecodingTimesFromBatch(MediaBuffer * /*buf*/,
             List<int64_t> &/*decodeTimeQueue*/) {}
 
-    virtual bool canDeferRelease(const sp<MetaData> &/*meta*/) { return false; }
-    virtual void setDeferRelease(sp<MetaData> &/*meta*/) {}
+    virtual bool canDeferRelease(const sp<MetaData> &meta) {
+        int32_t deferRelease = false;
+        return meta->findInt32(kKeyCanDeferRelease, &deferRelease) && deferRelease;
+    }
+
+    virtual void setDeferRelease(sp<MetaData> &meta) {
+        meta->setInt32(kKeyCanDeferRelease, true);
+    }
 
     virtual bool isAudioMuxFormatSupported(const char *mime);
     virtual void cacheCaptureBuffers(sp<hardware::ICamera> camera, video_encoder encoder);
